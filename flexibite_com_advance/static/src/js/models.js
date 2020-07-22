ab412dc817ca916a86220a0581bea706af76798a
Vladimir Alvarez <vladimir881002@gmail.com>
2020-07-22 18:31:32 +0200

    [FIX]Redondeando a entero la cantidad que se imprime en el ticket de venta del pos

--- flexibite_com_advance/static/src/js/models.js
+++ flexibite_com_advance/static/src/js/models.js
@@ -6,6 +6,7 @@ odoo.define('flexibite_com_advance.models', function (require) {
     var rpc = require('web.rpc');
     var _t = core._t;
     var utils = require('web.utils');
+    var field_utils = require('web.field_utils');
 
     var round_pr = utils.round_precision;
     var QWeb = core.qweb;
@@ -3476,7 +3477,36 @@ odoo.define('flexibite_com_advance.models', function (require) {
                 this.pos.get_order().remove_orderline(this);
                 return;
             }else{
-                _super_orderline.set_quantity.call(this, quantity, keep_price);
+                // _super_orderline.set_quantity.call(this, quantity, keep_price);
+                this.order.assert_editable();
+                if(quantity === 'remove'){
+                    this.order.remove_orderline(this);
+                    return;
+                }else{
+                    var quant = parseFloat(quantity) || 0;
+                    var unit = this.get_unit();
+                    if(unit){
+                        if (unit.rounding) {
+                            // var decimals = this.pos.dp['Product Unit of Measure'];
+                            var decimals = 0;
+                            var rounding = Math.max(unit.rounding, Math.pow(10, -decimals));
+                            this.quantity    = round_pr(quant, rounding);
+                            this.quantityStr = field_utils.format.float(this.quantity, {digits: [69, decimals]});
+                        } else {
+                            this.quantity    = round_pr(quant, 1);
+                            this.quantityStr = this.quantity.toFixed(0);
+                        }
+                    }else{
+                        this.quantity = quant;
+                        this.quantityStr = '' + this.quantity;
+                    }
+                }
+
+                // just like in sale.order changing the quantity will recompute the unit price
+                if(! keep_price && ! this.price_manually_set){
+                    this.set_unit_price(this.product.get_price(this.order.pricelist, this.get_quantity()));
+                    this.order.fix_tax_included_price(this);
+                }
             }
             this.trigger('change',this);
         },
