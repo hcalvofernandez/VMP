605b39b01612b083f354b7dafe967ec5bf438219
Vladimir Alvarez <vladimir881002@gmail.com>
2020-08-26 15:06:31 +0200

    Revert "[ADD] Agregando inicio directo en el pos utilizando el equipo de computo asociado"
    This reverts commit e500a9b08813149de48c3b3a9c0ee06c8e50bac9.

--- flexibite_com_advance/models/computer_equipment.py
+++ flexibite_com_advance/models/computer_equipment.py
@@ -10,10 +10,8 @@ _logger = logging.getLogger("___________________________________________________
 class ComputerEquipment(models.Model):
     _name = 'flexibite_com_advance.computer_equipment'
 
-    _sql_constraints = [('computer_equipment_ip_unique', 'unique(ip)', u'Ya existe un equipo de cómputo con ese ip.')]
-
     name = fields.Char('Nombre', required=True)
-    ip = fields.Char('Dirección IP',  required=True)
+    ip = fields.Char('Dirección IP')
 
 
 
