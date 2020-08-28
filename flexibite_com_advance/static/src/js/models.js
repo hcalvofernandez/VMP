ff3f9c4dbbbeadf36dd54386589a4a341f867cd7
Ernesto <riveronernesto90@gmail.com>
2020-08-28 15:35:22 -0500

    [FIX] Aumentando timeout para ordenes enviadas desde el TPV

--- flexibite_com_advance/static/src/js/models.js
+++ flexibite_com_advance/static/src/js/models.js
@@ -965,7 +965,11 @@ odoo.define('flexibite_com_advance.models', function (require) {
 //		},
         _save_to_server: function (orders, options) {
             var self = this;
-            arguments[1].timeout = 10500 * orders.length;
+            var timeout = 60000;
+            if (orders.length > 1){
+                timeout = 25000 * orders.length
+            }
+            arguments[1].timeout = timeout;
             return posmodel_super._save_to_server.apply(this, arguments)
             .done(function(server_ids){
                 _.each(orders, function(order) {
