de52efe5a6a3c04806d6756361d50e0bccf7af57
Jorge Luis López <jorgeluislr93@gmail.com>
2020-09-23 21:00:22 -0400

    [FIX]Se crearon dos grupos de acceso, para conceder los permisos de acceso a modificación del costo de inventario. [FIX]Se añadió un campo a la configuración del módulo de inventario, para configurar el margen de variación del costo del producto. [FIX]Se modificó la función write del modelo de product_template_attribute_value, para definir un validación que limita el acceso a modifcar el costo del producto. [FIX]Se modificó el archivo de traducción.

--- stock_variants_measure/models/__init__.py
+++ stock_variants_measure/models/__init__.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 from . import product_template
+from . import res_config_settings
 from . import uom_uom
 from . import product_template_attribute_value
 from . import sale_order_line
--- stock_variants_measure/models/product_template_attribute_value.py
+++ stock_variants_measure/models/product_template_attribute_value.py
@@ -1,5 +1,5 @@
 from odoo import api, fields, models, _
-from odoo.exceptions import Warning
+from odoo.exceptions import ValidationError
 from odoo.osv import osv
 from odoo.addons import decimal_precision as dp
 from odoo.http import request
@@ -9,7 +9,7 @@ class product_template_attribute_value(models.Model):
 
     cost_price = fields.Float('Precio Costo', default=0.0, help="Costo por compra.")
     variant_puom = fields.Selection(name="variant_puom", string='Unidad de medida', selection='_compute_product_variant_uom_selection', store=True)
-    variant_ratio = fields.Float('Rendimiento', digits = (12,3), default=0.0)
+    variant_ratio = fields.Float('Rendimiento', digits=(12, 3), default=0.0)
 
     _columns = {"variant_puom":fields.Selection(name="variant_puom", string='Unidad de medida', selection='_compute_product_variant_uom_selection')}
     
@@ -21,3 +21,26 @@ class product_template_attribute_value(models.Model):
             item = (str(unit.id) + str("-") + str(unit.name), unit.name)
             vector_uom_units.append(item)        
         return vector_uom_units
+
+    @api.multi
+    def write(self, values):
+        if 'cost_price' in values:
+            new_cost = values['cost_price']
+            group_user_ids = self.env.user.groups_id.mapped('id')
+            margin = float(self.env['ir.config_parameter'].get_param('stock_variants_measure.inflation_cost'))
+            group_user = self.env.ref('stock_variants_measure.group_inventory_cost_employee').id
+            group_manager = self.env.ref('stock_variants_measure.group_inventory_cost_manager').id
+            for attr in self:
+                if attr.cost_price != 0:
+                    difference = new_cost/attr.cost_price*100 - 100
+                else:
+                    difference = 100 #if cost_price field equal 0, it set the increment as 100%
+                if difference <= margin:
+                    if group_user in group_user_ids or group_manager in group_user_ids:
+                        continue
+                elif difference > margin:
+                    if group_manager in group_user_ids:
+                        continue
+                raise ValidationError(_("You do not have permission to set the new cost value for the product"))
+        return super(product_template_attribute_value, self).write(values)
+
