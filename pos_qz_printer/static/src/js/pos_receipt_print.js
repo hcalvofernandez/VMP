6e578108b439bb649664699dc35f06659de73327
jfernandez-890825 <juan.carlos.fdez.89@gmail.com>
2020-10-03 01:45:22 -0400

    Se modifica el código del módulo "pos_qz_printer" para que sea compatible con el módulo "flexibite_com_advance". Ya imprime en modo RAW.

--- pos_qz_printer/static/src/js/pos_receipt_print.js
+++ pos_qz_printer/static/src/js/pos_receipt_print.js
@@ -19,77 +19,108 @@ odoo.define('pos_qz_printer.pos_receipt_print', function (require) {
 
     var _t = core._t;
     var qzVersion = 0;
-    var data_to_print = ''
+    var data_to_print = '';
+
+    /// Authentication setup ///
+    qz.security.setCertificatePromise(function(resolve, reject) {
+        //Preferred method - from server
+//        $.ajax({ url: "assets/signing/digital-certificate.txt", cache: false, dataType: "text" }).then(resolve, reject);
+
+        //Alternate method 1 - anonymous
+//        resolve();
+
+        //Alternate method 2 - direct
+        resolve("-----BEGIN CERTIFICATE-----\n" +
+            "MIIFAzCCAuugAwIBAgICEAIwDQYJKoZIhvcNAQEFBQAwgZgxCzAJBgNVBAYTAlVT\n" +
+            "MQswCQYDVQQIDAJOWTEbMBkGA1UECgwSUVogSW5kdXN0cmllcywgTExDMRswGQYD\n" +
+            "VQQLDBJRWiBJbmR1c3RyaWVzLCBMTEMxGTAXBgNVBAMMEHF6aW5kdXN0cmllcy5j\n" +
+            "b20xJzAlBgkqhkiG9w0BCQEWGHN1cHBvcnRAcXppbmR1c3RyaWVzLmNvbTAeFw0x\n" +
+            "NTAzMTkwMjM4NDVaFw0yNTAzMTkwMjM4NDVaMHMxCzAJBgNVBAYTAkFBMRMwEQYD\n" +
+            "VQQIDApTb21lIFN0YXRlMQ0wCwYDVQQKDAREZW1vMQ0wCwYDVQQLDAREZW1vMRIw\n" +
+            "EAYDVQQDDAlsb2NhbGhvc3QxHTAbBgkqhkiG9w0BCQEWDnJvb3RAbG9jYWxob3N0\n" +
+            "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAtFzbBDRTDHHmlSVQLqjY\n" +
+            "aoGax7ql3XgRGdhZlNEJPZDs5482ty34J4sI2ZK2yC8YkZ/x+WCSveUgDQIVJ8oK\n" +
+            "D4jtAPxqHnfSr9RAbvB1GQoiYLxhfxEp/+zfB9dBKDTRZR2nJm/mMsavY2DnSzLp\n" +
+            "t7PJOjt3BdtISRtGMRsWmRHRfy882msBxsYug22odnT1OdaJQ54bWJT5iJnceBV2\n" +
+            "1oOqWSg5hU1MupZRxxHbzI61EpTLlxXJQ7YNSwwiDzjaxGrufxc4eZnzGQ1A8h1u\n" +
+            "jTaG84S1MWvG7BfcPLW+sya+PkrQWMOCIgXrQnAsUgqQrgxQ8Ocq3G4X9UvBy5VR\n" +
+            "CwIDAQABo3sweTAJBgNVHRMEAjAAMCwGCWCGSAGG+EIBDQQfFh1PcGVuU1NMIEdl\n" +
+            "bmVyYXRlZCBDZXJ0aWZpY2F0ZTAdBgNVHQ4EFgQUpG420UhvfwAFMr+8vf3pJunQ\n" +
+            "gH4wHwYDVR0jBBgwFoAUkKZQt4TUuepf8gWEE3hF6Kl1VFwwDQYJKoZIhvcNAQEF\n" +
+            "BQADggIBAFXr6G1g7yYVHg6uGfh1nK2jhpKBAOA+OtZQLNHYlBgoAuRRNWdE9/v4\n" +
+            "J/3Jeid2DAyihm2j92qsQJXkyxBgdTLG+ncILlRElXvG7IrOh3tq/TttdzLcMjaR\n" +
+            "8w/AkVDLNL0z35shNXih2F9JlbNRGqbVhC7qZl+V1BITfx6mGc4ayke7C9Hm57X0\n" +
+            "ak/NerAC/QXNs/bF17b+zsUt2ja5NVS8dDSC4JAkM1dD64Y26leYbPybB+FgOxFu\n" +
+            "wou9gFxzwbdGLCGboi0lNLjEysHJBi90KjPUETbzMmoilHNJXw7egIo8yS5eq8RH\n" +
+            "i2lS0GsQjYFMvplNVMATDXUPm9MKpCbZ7IlJ5eekhWqvErddcHbzCuUBkDZ7wX/j\n" +
+            "unk/3DyXdTsSGuZk3/fLEsc4/YTujpAjVXiA1LCooQJ7SmNOpUa66TPz9O7Ufkng\n" +
+            "+CoTSACmnlHdP7U9WLr5TYnmL9eoHwtb0hwENe1oFC5zClJoSX/7DRexSJfB7YBf\n" +
+            "vn6JA2xy4C6PqximyCPisErNp85GUcZfo33Np1aywFv9H+a83rSUcV6kpE/jAZio\n" +
+            "5qLpgIOisArj1HTM6goDWzKhLiR/AeG3IJvgbpr9Gr7uZmfFyQzUjvkJ9cybZRd+\n" +
+            "G8azmpBBotmKsbtbAU/I/LVk8saeXznshOVVpDRYtVnjZeAneso7\n" +
+            "-----END CERTIFICATE-----\n" +
+            "--START INTERMEDIATE CERT--\n" +
+            "-----BEGIN CERTIFICATE-----\n" +
+            "MIIFEjCCA/qgAwIBAgICEAAwDQYJKoZIhvcNAQELBQAwgawxCzAJBgNVBAYTAlVT\n" +
+            "MQswCQYDVQQIDAJOWTESMBAGA1UEBwwJQ2FuYXN0b3RhMRswGQYDVQQKDBJRWiBJ\n" +
+            "bmR1c3RyaWVzLCBMTEMxGzAZBgNVBAsMElFaIEluZHVzdHJpZXMsIExMQzEZMBcG\n" +
+            "A1UEAwwQcXppbmR1c3RyaWVzLmNvbTEnMCUGCSqGSIb3DQEJARYYc3VwcG9ydEBx\n" +
+            "emluZHVzdHJpZXMuY29tMB4XDTE1MDMwMjAwNTAxOFoXDTM1MDMwMjAwNTAxOFow\n" +
+            "gZgxCzAJBgNVBAYTAlVTMQswCQYDVQQIDAJOWTEbMBkGA1UECgwSUVogSW5kdXN0\n" +
+            "cmllcywgTExDMRswGQYDVQQLDBJRWiBJbmR1c3RyaWVzLCBMTEMxGTAXBgNVBAMM\n" +
+            "EHF6aW5kdXN0cmllcy5jb20xJzAlBgkqhkiG9w0BCQEWGHN1cHBvcnRAcXppbmR1\n" +
+            "c3RyaWVzLmNvbTCCAiIwDQYJKoZIhvcNAQEBBQADggIPADCCAgoCggIBANTDgNLU\n" +
+            "iohl/rQoZ2bTMHVEk1mA020LYhgfWjO0+GsLlbg5SvWVFWkv4ZgffuVRXLHrwz1H\n" +
+            "YpMyo+Zh8ksJF9ssJWCwQGO5ciM6dmoryyB0VZHGY1blewdMuxieXP7Kr6XD3GRM\n" +
+            "GAhEwTxjUzI3ksuRunX4IcnRXKYkg5pjs4nLEhXtIZWDLiXPUsyUAEq1U1qdL1AH\n" +
+            "EtdK/L3zLATnhPB6ZiM+HzNG4aAPynSA38fpeeZ4R0tINMpFThwNgGUsxYKsP9kh\n" +
+            "0gxGl8YHL6ZzC7BC8FXIB/0Wteng0+XLAVto56Pyxt7BdxtNVuVNNXgkCi9tMqVX\n" +
+            "xOk3oIvODDt0UoQUZ/umUuoMuOLekYUpZVk4utCqXXlB4mVfS5/zWB6nVxFX8Io1\n" +
+            "9FOiDLTwZVtBmzmeikzb6o1QLp9F2TAvlf8+DIGDOo0DpPQUtOUyLPCh5hBaDGFE\n" +
+            "ZhE56qPCBiQIc4T2klWX/80C5NZnd/tJNxjyUyk7bjdDzhzT10CGRAsqxAnsjvMD\n" +
+            "2KcMf3oXN4PNgyfpbfq2ipxJ1u777Gpbzyf0xoKwH9FYigmqfRH2N2pEdiYawKrX\n" +
+            "6pyXzGM4cvQ5X1Yxf2x/+xdTLdVaLnZgwrdqwFYmDejGAldXlYDl3jbBHVM1v+uY\n" +
+            "5ItGTjk+3vLrxmvGy5XFVG+8fF/xaVfo5TW5AgMBAAGjUDBOMB0GA1UdDgQWBBSQ\n" +
+            "plC3hNS56l/yBYQTeEXoqXVUXDAfBgNVHSMEGDAWgBQDRcZNwPqOqQvagw9BpW0S\n" +
+            "BkOpXjAMBgNVHRMEBTADAQH/MA0GCSqGSIb3DQEBCwUAA4IBAQAJIO8SiNr9jpLQ\n" +
+            "eUsFUmbueoxyI5L+P5eV92ceVOJ2tAlBA13vzF1NWlpSlrMmQcVUE/K4D01qtr0k\n" +
+            "gDs6LUHvj2XXLpyEogitbBgipkQpwCTJVfC9bWYBwEotC7Y8mVjjEV7uXAT71GKT\n" +
+            "x8XlB9maf+BTZGgyoulA5pTYJ++7s/xX9gzSWCa+eXGcjguBtYYXaAjjAqFGRAvu\n" +
+            "pz1yrDWcA6H94HeErJKUXBakS0Jm/V33JDuVXY+aZ8EQi2kV82aZbNdXll/R6iGw\n" +
+            "2ur4rDErnHsiphBgZB71C5FD4cdfSONTsYxmPmyUb5T+KLUouxZ9B0Wh28ucc1Lp\n" +
+            "rbO7BnjW\n" +
+            "-----END CERTIFICATE-----\n");
+    });
+
+    qz.security.setSignaturePromise(function(toSign) {
+        return function(resolve, reject) {
+            //Preferred method - from server
+//            $.ajax("/secure/url/for/sign-message?request=" + toSign).then(resolve, reject);
+
+            //Alternate method - unsigned
+            resolve();
+        };
+    });
 
     function findVersion() {
         qz.api.getVersion().then(function(data) {
             qzVersion = data;
+            findPrinters();
+        }).catch(function(err) {
+            alert(err || 'Connection failed');
         });
     }
 
     function startConnection(config) {
-        qz.security.setCertificatePromise(function(resolve, reject) {
-            $.ajax("/pos_qz_printer/static/src/lib/digital-certificate.txt").then(resolve, reject);
-        });
-
-        var privateKey = "-----BEGIN RSA PRIVATE KEY-----\n" +
-            "MIIEpQIBAAKCAQEAwRc05UhbsKtU/SupjO8HHrVKKwglsfJeBoUMQoHo41a440Do\n" +
-            "r6dbVI/HJITAQ1swIJjwmD9QqSVesnHnc7e6zlkj1ff1fDsOomIzX2SnB2CA9eiw\n" +
-            "5cfsXth6grZ6NIr7fc9NzyDpl3XcCEE+2ijbZCB0hWIVRkFBYh+RJPnoEFtb8njM\n" +
-            "J9V/YgXQf969jIFjAS8QVsDBvnnSsTeoE/2AXs1tRO4bzPEF65UouVeKJfBcICK4\n" +
-            "T7ZMObJEKAHc/PMLd9pLBG9Gg4/59AoeWuM92qti1i3307WFGKKGNNZ5Tt/EeX+2\n" +
-            "5LPu8yilRK+F3hlpvvTNzwK7KAvoNWBCQll2rQIDAQABAoIBACyBrt2Smh/UvhhE\n" +
-            "8iXcCqYXX2sfy6CCnw2dqT/DNe0A1kj7cybZyoFpSpuuRarA4A0Dc6GEJpF2Xad/\n" +
-            "/bt8hACAJ3RwXRMvgaYIQJMiXiWjJtaHtg6g0GjkOQjcCrsFtgY/vE2b5nvU3MzC\n" +
-            "TTx34mnn2TPNcd3puKpnYEtHlyf9oBEKOE85gyOv1fMUZeQw/kPSNEr+gWQnj/u6\n" +
-            "rchlzPhHZjmMuB5At6/yWURnjbFuYwgb2djjDNY52KEcCGJvDsrrFqs5EKE40u96\n" +
-            "CNNtQNAye5mT89Jl2JwPJobpsycDEqZEayc6kJX/77e/2Y3JIuY9gm+Q17opHkYg\n" +
-            "7IbQP7kCgYEA8ZC48g01YaCTk2DoRIXF0hRb8MbjeVs5ej20l7GbicWuF3u4LNld\n" +
-            "vQgFhC2xiIFl39HwoRGHdN/NYo5TZcGsnScsItM4gIlZCruIpaj9wWiKbDwWf2p7\n" +
-            "V8+H8KrSsqaX1Jy2mieG/kgdXI4bqPCh74sEjw1g6XTboYBFsrH1s18CgYEAzKD0\n" +
-            "33f42BVie/p8ta+tqXNOsr4U/2czZU8ZSievheZcMyoQLmsJcIiOO4eZ55h5MR7d\n" +
-            "bL6XaIfXrpuaLANkx2wi5PEOtp5fIT4u+AJb6DQdcRfYZ3VMkG00b2hSCSUbrWho\n" +
-            "9x9wQaGC1RKj+XBAUgydXQFdXZi8sOApmTpr/XMCgYEAhfJt2yof04aqzioKIRTc\n" +
-            "YGURpi1irUQ8VuAoZ4UAbiDDLBpaQeQ16j+sb2K28q5twvIyr918cv42cNPiwqXm\n" +
-            "BS5XdugQiJWgXicm2lUegERrnSCkiPqOcl6NTpIqSw29WxOa3VfVruJmBZB3HfJw\n" +
-            "mNdJK9mLR2iY8LCj9TZgu5kCgYEApwBdmNui3UdmnuQpT2ZXBsoyWjJDlMW27mGF\n" +
-            "tD17RH5ilOcpWZjFlW/9FJxwgNCxZ+NWtt89VnQ3FCutwWnrn82jFNGfPm82GD1V\n" +
-            "u9bBB1sxBBF/7b+Pgvd9Kccr3IbKddWWhMjFpuqXiimyZWq1M8FT1Im+lxqGNJxd\n" +
-            "ls5VP/0CgYEA5S/gZ1gPGgUE18R4cMNDstTA90QFC15yzkRpLtth9DPoKoX/r2G0\n" +
-            "8eKN9tIBY+VBEf00g6stSN02ncq5LKE/tS1OO4OmM+u6G/qjPAoW2AlPMqnptyFo\n" +
-            "kFEGUC6AlTfS2E5WfF38SdwsBi6c2QIFBP2GKAjo5pC9WglhnVvVPBE=\n" +
-            "-----END RSA PRIVATE KEY-----\n";
-
-        qz.security.setSignaturePromise(function(toSign) {
-            return function(resolve, reject) {
-                try {
-                    var pk = new RSAKey();
-                    pk.readPrivateKeyFromPEMString(strip(privateKey));
-                    var hex = pk.signString(toSign, 'sha1');
-                    console.log("DEBUG: \n\n" + stob64(hextorstr(hex)));
-                    resolve(stob64(hextorstr(hex)));
-                } catch (err) {
-                    console.error(err);
-                    reject(err);
-                }
-            };
-        });
-
-        function strip(key) {
-            if (key.indexOf('-----') !== -1) {
-                return key.split('-----')[2].replace(/\r?\n|\r/g, '');
-            }
-        }
-
         if (!qz.websocket.isActive()) {
-            console.log('Waiting default');
             qz.websocket.connect(config).then(function() {
-                console.log('Active success');
+                console.log('Connection success');
                 findVersion();
-                findPrinters();
+            }).catch(function(err) {
+                alert(err || 'Connection failed');
             });
         } else {
-            console.log('An active connection with QZ already exists.', 'alert-warning');
+            findPrinters();
         }
     }
 
@@ -99,6 +130,7 @@ odoo.define('pos_qz_printer.pos_receipt_print', function (require) {
             qz.printers.find(company[0].pos_printer).then(function(data) {
                 console.log("Found: " + data);
                 setPrinter(data);
+                printReceipt();
             }).catch(function(err) {
                 console.log("Found Printer Error:", err);
             });
@@ -108,25 +140,8 @@ odoo.define('pos_qz_printer.pos_receipt_print', function (require) {
     function setPrinter(printer) {
         var cf = getUpdatedConfig();
         cf.setPrinter(printer);
-        if (typeof printer === 'object' && printer.name == undefined) {
-            var shown;
-            if (printer.file != undefined) {
-                shown = "<em>FILE:</em> " + printer.file;
-            }
-            if (printer.host != undefined) {
-                shown = "<em>HOST:</em> " + printer.host + ":" + printer.port;
-            }
-        } else {
-            if (printer.name != undefined) {
-                printer = printer.name;
-            }
-
-            if (printer == undefined) {
-                printer = 'NONE';
-            }
-            printReceipt();
-        }
     }
+
     /// QZ Config ///
     var cfg = null;
 
@@ -142,35 +157,20 @@ odoo.define('pos_qz_printer.pos_receipt_print', function (require) {
         });
         return cfg
     }
+
     function printReceipt() {
         var config = getUpdatedConfig();
-        var printData =
-            [
-                data_to_print
-            ];
-        console.log("ddddddddddddd", printData)
-        qz.print(config, printData).catch(function(e) { console.error(e); });
-        location.reload();
+
+        var printData = [
+            { type: 'raw', format: 'plain', data: data_to_print }
+        ];
+
+        qz.print(config, printData).catch(function(err) {
+            alert(err || 'Connection failed');
+        });
     }
+
     screens.ReceiptScreenWidget.include({
-        // print: function() {
-        //     var self = this;
-        //     if(self.template != 'ReceiptScreenWidget'){
-        //         self._super();
-        //         return;
-        //     }
-        //     company_id = self.pos.company.id
-        //     var order = self.pos.get_order();
-        //     var receipt = self.$('.pos-receipt-container').html(QWeb.render('PosTicket',{
-        //             widget:self,
-        //             order: order,
-        //             receipt: order.export_for_printing(),
-        //             orderlines: order.get_orderlines(),
-        //             paymentlines: order.get_paymentlines(),
-        //         }));
-        //     data_to_print = receipt[0].outerHTML
-        //     startConnection();
-        // },
         print: function() {
             company_id = this.pos.company.id;
             data_to_print = '';
