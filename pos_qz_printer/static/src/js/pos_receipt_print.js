9b8b83b3b9da50c9c87303fd2253291a18283482
jcfernandez-890825 <juan.carlos.fdez.89@gmail.com>
2020-10-06 00:12:36 -0400

    Se modifica el código del módulo "pos_qz_printer" para que solo pida el certificado una vez. Se marca el checkbox para aceptar el certificado y el mensaje no se muestra más.

--- pos_qz_printer/static/src/js/pos_receipt_print.js
+++ pos_qz_printer/static/src/js/pos_receipt_print.js
@@ -16,76 +16,76 @@ odoo.define('pos_qz_printer.pos_receipt_print', function (require) {
 
         //Alternate method 2 - direct
         resolve("-----BEGIN CERTIFICATE-----\n" +
-            "MIIFAzCCAuugAwIBAgICEAIwDQYJKoZIhvcNAQEFBQAwgZgxCzAJBgNVBAYTAlVT\n" +
-            "MQswCQYDVQQIDAJOWTEbMBkGA1UECgwSUVogSW5kdXN0cmllcywgTExDMRswGQYD\n" +
-            "VQQLDBJRWiBJbmR1c3RyaWVzLCBMTEMxGTAXBgNVBAMMEHF6aW5kdXN0cmllcy5j\n" +
-            "b20xJzAlBgkqhkiG9w0BCQEWGHN1cHBvcnRAcXppbmR1c3RyaWVzLmNvbTAeFw0x\n" +
-            "NTAzMTkwMjM4NDVaFw0yNTAzMTkwMjM4NDVaMHMxCzAJBgNVBAYTAkFBMRMwEQYD\n" +
-            "VQQIDApTb21lIFN0YXRlMQ0wCwYDVQQKDAREZW1vMQ0wCwYDVQQLDAREZW1vMRIw\n" +
-            "EAYDVQQDDAlsb2NhbGhvc3QxHTAbBgkqhkiG9w0BCQEWDnJvb3RAbG9jYWxob3N0\n" +
-            "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAtFzbBDRTDHHmlSVQLqjY\n" +
-            "aoGax7ql3XgRGdhZlNEJPZDs5482ty34J4sI2ZK2yC8YkZ/x+WCSveUgDQIVJ8oK\n" +
-            "D4jtAPxqHnfSr9RAbvB1GQoiYLxhfxEp/+zfB9dBKDTRZR2nJm/mMsavY2DnSzLp\n" +
-            "t7PJOjt3BdtISRtGMRsWmRHRfy882msBxsYug22odnT1OdaJQ54bWJT5iJnceBV2\n" +
-            "1oOqWSg5hU1MupZRxxHbzI61EpTLlxXJQ7YNSwwiDzjaxGrufxc4eZnzGQ1A8h1u\n" +
-            "jTaG84S1MWvG7BfcPLW+sya+PkrQWMOCIgXrQnAsUgqQrgxQ8Ocq3G4X9UvBy5VR\n" +
-            "CwIDAQABo3sweTAJBgNVHRMEAjAAMCwGCWCGSAGG+EIBDQQfFh1PcGVuU1NMIEdl\n" +
-            "bmVyYXRlZCBDZXJ0aWZpY2F0ZTAdBgNVHQ4EFgQUpG420UhvfwAFMr+8vf3pJunQ\n" +
-            "gH4wHwYDVR0jBBgwFoAUkKZQt4TUuepf8gWEE3hF6Kl1VFwwDQYJKoZIhvcNAQEF\n" +
-            "BQADggIBAFXr6G1g7yYVHg6uGfh1nK2jhpKBAOA+OtZQLNHYlBgoAuRRNWdE9/v4\n" +
-            "J/3Jeid2DAyihm2j92qsQJXkyxBgdTLG+ncILlRElXvG7IrOh3tq/TttdzLcMjaR\n" +
-            "8w/AkVDLNL0z35shNXih2F9JlbNRGqbVhC7qZl+V1BITfx6mGc4ayke7C9Hm57X0\n" +
-            "ak/NerAC/QXNs/bF17b+zsUt2ja5NVS8dDSC4JAkM1dD64Y26leYbPybB+FgOxFu\n" +
-            "wou9gFxzwbdGLCGboi0lNLjEysHJBi90KjPUETbzMmoilHNJXw7egIo8yS5eq8RH\n" +
-            "i2lS0GsQjYFMvplNVMATDXUPm9MKpCbZ7IlJ5eekhWqvErddcHbzCuUBkDZ7wX/j\n" +
-            "unk/3DyXdTsSGuZk3/fLEsc4/YTujpAjVXiA1LCooQJ7SmNOpUa66TPz9O7Ufkng\n" +
-            "+CoTSACmnlHdP7U9WLr5TYnmL9eoHwtb0hwENe1oFC5zClJoSX/7DRexSJfB7YBf\n" +
-            "vn6JA2xy4C6PqximyCPisErNp85GUcZfo33Np1aywFv9H+a83rSUcV6kpE/jAZio\n" +
-            "5qLpgIOisArj1HTM6goDWzKhLiR/AeG3IJvgbpr9Gr7uZmfFyQzUjvkJ9cybZRd+\n" +
-            "G8azmpBBotmKsbtbAU/I/LVk8saeXznshOVVpDRYtVnjZeAneso7\n" +
-            "-----END CERTIFICATE-----\n" +
-            "--START INTERMEDIATE CERT--\n" +
-            "-----BEGIN CERTIFICATE-----\n" +
-            "MIIFEjCCA/qgAwIBAgICEAAwDQYJKoZIhvcNAQELBQAwgawxCzAJBgNVBAYTAlVT\n" +
-            "MQswCQYDVQQIDAJOWTESMBAGA1UEBwwJQ2FuYXN0b3RhMRswGQYDVQQKDBJRWiBJ\n" +
-            "bmR1c3RyaWVzLCBMTEMxGzAZBgNVBAsMElFaIEluZHVzdHJpZXMsIExMQzEZMBcG\n" +
-            "A1UEAwwQcXppbmR1c3RyaWVzLmNvbTEnMCUGCSqGSIb3DQEJARYYc3VwcG9ydEBx\n" +
-            "emluZHVzdHJpZXMuY29tMB4XDTE1MDMwMjAwNTAxOFoXDTM1MDMwMjAwNTAxOFow\n" +
-            "gZgxCzAJBgNVBAYTAlVTMQswCQYDVQQIDAJOWTEbMBkGA1UECgwSUVogSW5kdXN0\n" +
-            "cmllcywgTExDMRswGQYDVQQLDBJRWiBJbmR1c3RyaWVzLCBMTEMxGTAXBgNVBAMM\n" +
-            "EHF6aW5kdXN0cmllcy5jb20xJzAlBgkqhkiG9w0BCQEWGHN1cHBvcnRAcXppbmR1\n" +
-            "c3RyaWVzLmNvbTCCAiIwDQYJKoZIhvcNAQEBBQADggIPADCCAgoCggIBANTDgNLU\n" +
-            "iohl/rQoZ2bTMHVEk1mA020LYhgfWjO0+GsLlbg5SvWVFWkv4ZgffuVRXLHrwz1H\n" +
-            "YpMyo+Zh8ksJF9ssJWCwQGO5ciM6dmoryyB0VZHGY1blewdMuxieXP7Kr6XD3GRM\n" +
-            "GAhEwTxjUzI3ksuRunX4IcnRXKYkg5pjs4nLEhXtIZWDLiXPUsyUAEq1U1qdL1AH\n" +
-            "EtdK/L3zLATnhPB6ZiM+HzNG4aAPynSA38fpeeZ4R0tINMpFThwNgGUsxYKsP9kh\n" +
-            "0gxGl8YHL6ZzC7BC8FXIB/0Wteng0+XLAVto56Pyxt7BdxtNVuVNNXgkCi9tMqVX\n" +
-            "xOk3oIvODDt0UoQUZ/umUuoMuOLekYUpZVk4utCqXXlB4mVfS5/zWB6nVxFX8Io1\n" +
-            "9FOiDLTwZVtBmzmeikzb6o1QLp9F2TAvlf8+DIGDOo0DpPQUtOUyLPCh5hBaDGFE\n" +
-            "ZhE56qPCBiQIc4T2klWX/80C5NZnd/tJNxjyUyk7bjdDzhzT10CGRAsqxAnsjvMD\n" +
-            "2KcMf3oXN4PNgyfpbfq2ipxJ1u777Gpbzyf0xoKwH9FYigmqfRH2N2pEdiYawKrX\n" +
-            "6pyXzGM4cvQ5X1Yxf2x/+xdTLdVaLnZgwrdqwFYmDejGAldXlYDl3jbBHVM1v+uY\n" +
-            "5ItGTjk+3vLrxmvGy5XFVG+8fF/xaVfo5TW5AgMBAAGjUDBOMB0GA1UdDgQWBBSQ\n" +
-            "plC3hNS56l/yBYQTeEXoqXVUXDAfBgNVHSMEGDAWgBQDRcZNwPqOqQvagw9BpW0S\n" +
-            "BkOpXjAMBgNVHRMEBTADAQH/MA0GCSqGSIb3DQEBCwUAA4IBAQAJIO8SiNr9jpLQ\n" +
-            "eUsFUmbueoxyI5L+P5eV92ceVOJ2tAlBA13vzF1NWlpSlrMmQcVUE/K4D01qtr0k\n" +
-            "gDs6LUHvj2XXLpyEogitbBgipkQpwCTJVfC9bWYBwEotC7Y8mVjjEV7uXAT71GKT\n" +
-            "x8XlB9maf+BTZGgyoulA5pTYJ++7s/xX9gzSWCa+eXGcjguBtYYXaAjjAqFGRAvu\n" +
-            "pz1yrDWcA6H94HeErJKUXBakS0Jm/V33JDuVXY+aZ8EQi2kV82aZbNdXll/R6iGw\n" +
-            "2ur4rDErnHsiphBgZB71C5FD4cdfSONTsYxmPmyUb5T+KLUouxZ9B0Wh28ucc1Lp\n" +
-            "rbO7BnjW\n" +
-            "-----END CERTIFICATE-----\n");
+            "MIIEJzCCAw+gAwIBAgIUZBiEqLcWY/TFyR/k7iO+jczNN2MwDQYJKoZIhvcNAQEL\n" +
+            "BQAwgaExCzAJBgNVBAYTAkNVMQ8wDQYDVQQIDAZIYXZhbmExFDASBgNVBAcMC0hh\n" +
+            "dmFuYSBDaXR5MRgwFgYDVQQKDA9PZG9vIEV4cGVydHMgTVgxEDAOBgNVBAsMB0Rl\n" +
+            "dmVsb3AxEjAQBgNVBAMMCWxvY2FsaG9zdDErMCkGCSqGSIb3DQEJARYcanVhbi5j\n" +
+            "YXJsb3MuZmRlei44OWdtYWlsLmNvbTAgFw0yMDEwMDYwMTA2MzRaGA8yMDUyMDMz\n" +
+            "MTAxMDYzNFowgaExCzAJBgNVBAYTAkNVMQ8wDQYDVQQIDAZIYXZhbmExFDASBgNV\n" +
+            "BAcMC0hhdmFuYSBDaXR5MRgwFgYDVQQKDA9PZG9vIEV4cGVydHMgTVgxEDAOBgNV\n" +
+            "BAsMB0RldmVsb3AxEjAQBgNVBAMMCWxvY2FsaG9zdDErMCkGCSqGSIb3DQEJARYc\n" +
+            "anVhbi5jYXJsb3MuZmRlei44OWdtYWlsLmNvbTCCASIwDQYJKoZIhvcNAQEBBQAD\n" +
+            "ggEPADCCAQoCggEBAMlBeIXH+LIS6hStsIgttPuE8bZiAuzlAvNqhXryrcibyD/7\n" +
+            "6ciD4XJ2IUqmb7Zrmq8bmrVzvMg7OZOILk8PHYkPiFjASu/4t0evC+z+cXFdcycB\n" +
+            "vlJSCyPpZTIKX5UeWspFeTgaqdcmJ4fNBtadm2R8JiaZgrQrIzL6Q5S9RMVTDRlU\n" +
+            "BT6hy8J+INtFA5yN+OwmW6NgXcjQqiQZeMEqW0dzh0EnaNj0rGb6AWtBO4a9YyVD\n" +
+            "o3BZVFJXFYaDjvQu+d62c9JEaw3RIOcmdwaiOXdIfvRg7NVXsc0dyuRe/vZyiyTl\n" +
+            "7T2B3QrZ/SEN2f3gKqjcpd+gna4DG7I1K2AgH3ECAwEAAaNTMFEwHQYDVR0OBBYE\n" +
+            "FA7Qo9waJBZr79JyN4NFKUjZiPxeMB8GA1UdIwQYMBaAFA7Qo9waJBZr79JyN4NF\n" +
+            "KUjZiPxeMA8GA1UdEwEB/wQFMAMBAf8wDQYJKoZIhvcNAQELBQADggEBAHhJrCTs\n" +
+            "T7S9Id7Eug7T7LKnUu0tmDzDTXlXKCYcEzdCUZmmgz7YAGPSIljad+6Q6/gPtr4H\n" +
+            "Ywk/7SIESW6X4SQnwUBrkVjerN1GFUimW8d2ubX3lxe82lxQgPNBMBhTLQe+Epys\n" +
+            "kEUxdp6hH9x3NvtRBnSbE+KI1MFWDk9d+9Jh+jQJXRZbMS/BJFnVY9Eb2n/RIH/U\n" +
+            "2sNGOnpjZZkAek4nmWpOqGIIWBfhgaQYWTPEWhUpJpMqVYrCg9/hgq1GZoAiujeU\n" +
+            "Z6WuBNbG7cykdPR7QzM7PnKqAfGD08OHmiVsS+Egno7sWeCP/OogBm2L+LB2nJqu\n" +
+            "8swChJSQx7mAuVE=\n" +
+            "-----END CERTIFICATE-----");
     });
+
+    let privateKey = "-----BEGIN PRIVATE KEY-----\n" +
+        "MIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQDJQXiFx/iyEuoU\n" +
+        "rbCILbT7hPG2YgLs5QLzaoV68q3Im8g/++nIg+FydiFKpm+2a5qvG5q1c7zIOzmT\n" +
+        "iC5PDx2JD4hYwErv+LdHrwvs/nFxXXMnAb5SUgsj6WUyCl+VHlrKRXk4GqnXJieH\n" +
+        "zQbWnZtkfCYmmYK0KyMy+kOUvUTFUw0ZVAU+ocvCfiDbRQOcjfjsJlujYF3I0Kok\n" +
+        "GXjBKltHc4dBJ2jY9Kxm+gFrQTuGvWMlQ6NwWVRSVxWGg470LvnetnPSRGsN0SDn\n" +
+        "JncGojl3SH70YOzVV7HNHcrkXv72cosk5e09gd0K2f0hDdn94Cqo3KXfoJ2uAxuy\n" +
+        "NStgIB9xAgMBAAECggEATGQZh048S/t59pdnJomS5NW1mfDiA2F6GUMZn212IWtg\n" +
+        "O0k3l/DXJkdIo7lBNhDqCgWEV9OymJ5CYImUalXmdBv9ORLkXuodR4xeDnsWPF2o\n" +
+        "U+oJf928dMkmrDPbzBJVi+mK9YzJYu0MnDNqoRP5485r/MLQJesp1TPBazfcXrlW\n" +
+        "Hag+oJXdBq6N6yedFQ8YKplL6jUvomHOWTE7dOrXCCvMp33+C1AV1UwngB5xrjSl\n" +
+        "vB9pq+0lJ2tLeFckY9ZcBqr0KUzbzBCxkaa1rzF8x5+mvgr3H2I3Bwplw/qx8jLh\n" +
+        "pBnyY9p2nWm0MDaHG1CSgtKopKpWOKZiQ2LIUZd5QQKBgQD941g1SmCB9ZaDgmqJ\n" +
+        "OTk3JS6alUyOYR3oceXP5HpTzD/vh9CbZg6ZF3+oVq1VSAREo/ZpDmzo/ZxTXBGV\n" +
+        "ogKSmTtbbiu/hgAFiEiMMym2bvpKE3pgAgMrC0cptkf+pZt0XGK/c1+50K0nadBG\n" +
+        "Hb6v3Z/sPBYAJSfG5JVTmHBXpQKBgQDK7guoWHwYZdV/CFyYeNuuNPGDMLnuCSEc\n" +
+        "OfcuycUGAnAYc57ykMMS94fh/VdUEHjHuRvFe6nN9FwNFp5M2oWmE85h6FirRieo\n" +
+        "iiCmuI7StwB22OWsdOLD1PcPBdOYfZO+/ZPIluqU7IlF6NtI2v0SXdoZ7cVePAnG\n" +
+        "kBK8Mji+3QKBgQDr9ZgsZwTMo+AQN2wmPt2zJ8ZpeuqcxRvTxc9taI5cRU8Cono5\n" +
+        "gx98Hv/RB0WvJfB5jt1bhEGqRNI+EypwpBK6BtnIvtq/9J0ehyvNyVsyJ3BM2k94\n" +
+        "3UXuglvQI45D18xj190ay/KispPLNqeQEuL/df3Ew4868U1Hp3g1Kk721QKBgBTj\n" +
+        "b/vCL/Pq95D98VoYhrRl6r5QU7M26VuYc+MzDb7SnWtG64OYdxwKssgGZejXUTdt\n" +
+        "zHgFhLMYhikqvNy4Wa2AuGvHmOj4bbihjz7aHImAIOIa7XlVBsqVr2CyftdhhcXR\n" +
+        "IFczga4WswKpN3lGYWp0krC8kuZiU7dGuUb5/HFZAoGAMznKFt44cUIhr0726P+0\n" +
+        "rWI3+zJmCVKDdoHLPgKlEd+V7VYMdHsgf5B/JXctNYwOZlCR6W0/wUqkbBvDZVXj\n" +
+        "ykDCjCifaWWPPsGaIaEp1KW0QYOox+AA8Y7NUoWr7+Za/jF2iZGl2FvVmu7Q7nB8\n" +
+        "3y70XeowmZ1/UUf0hMw6pYQ=\n" +
+        "-----END PRIVATE KEY-----";
 
     qz.security.setSignatureAlgorithm("SHA512"); // Since 2.1
     qz.security.setSignaturePromise(function(toSign) {
         return function(resolve, reject) {
-            //Preferred method - from server
-//            fetch("/secure/url/for/sign-message?request=" + toSign, {cache: 'no-store', headers: {'Content-Type': 'text/plain'}})
-//              .then(function(data) { data.ok ? resolve(data.text()) : reject(data.text()); });
-
-            //Alternate method - unsigned
-            resolve(); // remove this line in live environment
+            try {
+                let pk = KEYUTIL.getKey(privateKey);
+                let sig = new KJUR.crypto.Signature({"alg": "SHA512withRSA"});  // Use "SHA1withRSA" for QZ Tray 2.0 and older
+                sig.init(pk);
+                sig.updateString(toSign);
+                let hex = sig.sign();
+                console.log("DEBUG: \n\n" + stob64(hextorstr(hex)));
+                resolve(stob64(hextorstr(hex)));
+            } catch (err) {
+                console.error(err);
+                reject(err);
+            }
         };
     });
 
