7279186ee719f3a24705a193dbebf2bc6dc44bcf
jcfernandez-890825 <juan.carlos.fdez.89@gmail.com>
2020-10-25 01:46:46 -0400

    El nombre de la compañía cambió a "(39) DELI SAZON JAROPAMEX"

--- from_jaropamex_copy_expensed_product/hooks.py
+++ from_jaropamex_copy_expensed_product/hooks.py
@@ -1,5 +1,4 @@
 # -*- coding: utf-8 -*-
-# unlink_doc
 from odoo import SUPERUSER_ID, api
 
 
@@ -17,17 +16,17 @@ def post_init_hook(cr, registry):
         ('name', '=', '(09) Mendocinos Madero')
     ]).ensure_one()
 
-    # Obtener productos QUE puedan ser comprados y que puedan ser un gasto de "(39) DeliSazon Jaropamex Sabinas"
+    # Obtener productos QUE puedan ser comprados y que puedan ser un gasto de "(39) DELI SAZON JAROPAMEX"
     for expensed_product in env['product.template'].search([
-        ('company_id.name', '=', '(39) DeliSazon Jaropamex Sabinas'),
+        ('company_id.name', '=', '(39) DELI SAZON JAROPAMEX'),
         ('can_be_expensed', '=', True), ('purchase_ok', '=', True),
     ]):
-        # Copiar a insumo a "(31) Mendocinos Tampico"
+        # Copiar a producto a "(31) Mendocinos Tampico"
         expensed_product.copy({
             'name': expensed_product.name,
             'company_id': tampico_company.id,
         })
-        # Copiar a insumo a "(09) Mendocinos Madero"
+        # Copiar a producto a "(09) Mendocinos Madero"
         expensed_product.copy({
             'name': expensed_product.name,
             'company_id': madero_company.id,
